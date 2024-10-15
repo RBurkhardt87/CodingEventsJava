a8a10abae8feedc050c871f9f73319ad2f22ce3b
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-10-15 14:05:42 -0500

    Created my own validation branch with notes for follow along video

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
@@ -16,26 +18,100 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    //TODO: MODEL BINDING ::::: AND ADDING VALIDATION
+    //* IT IS IMPORTANT TO UNDERSTAND THAT WE NEED VALIDATION ON BOTH THE USER AND SERVER SIDE. WE CREATE ANNOTATIONS FOR OUR FIELDS ON THE MODEL END OF IT, BUT WE
+    //* ALSO NEED TO ADDRESS VALIDATION INSIDE OUR CONTROLLERS TO MAKE SURE THAT THOSE ANNOTATIONS ARE BEING READ AND CARRIED OUT BY SPRING BOOT. AND ONCE THEY ARE,
+    //* WE NEED CREATE ERROR MESSAGES THAT CAN HELP GUIDE THE USER TO UNDERSTAND HOW TO FIX ANY DATA MISTAKES. MAKE IT MORE USER FRIENDLY...
+
+
     @GetMapping
-    public String displayAllEvents(Model model) {
+    public String displayEvents(Model model){
         model.addAttribute("title", "All Events");
         model.addAttribute("events", EventData.getAll());
         return "events/index";
+
     }
 
+    //lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
+    public String displayCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
+
+    //TODO: Replace the @RequestParam for eventName and eventDescription to be... @ModelAttribute Event newEvent
+    //It will look at the request data and look for fields that match the identifiers
+    //We can then just add an event by doing .add(newEvent)   Spring will do the work and create event for us because of the @ModelAttribute
+    //? Although we added contactEmail, we don't have to do anything here in the controller because of Model Binding. As long as the email is apart of the form
+    //? And is named according to the field, this doesn't need to be updated with anything.
+
+    //? The Model validation is meant to define what the validation params are. It is meant to determine what good data looks like.
+    //? The Controller is responsible for checking those criteria.
+    //? By adding the @Valid annotation, it says that not only should Spring Boot do Model Binding when it comes in, but it should check any validation annotations on the fields
+    //? for this class and make sure that they are satisfied. ---> w/o @Valid on the controller. Our validation annotations on the fields won't work. However, it returns a nasty error display So... we must fix that
+    //! The Errors object added into the params will be populated by any validation errors that occur when Spring Boot does validation according to the annotations we assigned on our fields
+    //! If there are errors, we want to send the user back to the create event form, which is above this... Because we are sending a "title", we must add the Model model like above.
+    //! So, this is saying that if there are errors send to the @GetMapping handler displayCreateEventForm, and if no errors, create the event and redirect to the events page.
+    //* After we created that, it stopped displaying the funky error page and instead just had user start back at the createEventForm. But without any message displayed. So we added
+    //* "errorMsg" to our model and now we will head over to our create.html template (since that's where we are sending them back to) to get it functioning for us...
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad Data!");
+            return "events/create";
+        }
+
         EventData.add(newEvent);
         return "redirect:/events";
     }
 
-    @GetMapping("delete")
-    public String displayDeletedEventForm(Model Model)
 
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:/events";
+    }
+
+
+
+    //TODO: Create a method to display an edit form
+    //PathVariable---> the eventId param goes inside {}
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title);
+        return "/events/edit";
+    }
+
+    //TODO: Create a method to process the Edit Form
+    //Here we are using setters to set the args passed in as the values of the fields of the event
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect:/events";
+    }
+
+    //Get Handlers typically display the forms and the Post Handlers are going to process the forms
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -11,10 +11,19 @@ import java.util.Objects;
  */
 public class Event {
 
+    //TODO: create unique id property
     private int id;
+
+    //TODO: create a nextId -- nextId is declared static, therefore, every single object of Event will share it
+    //By initializing to 1, that means the first object will get the id of #1. And then we will update the nextId inside the constructor
+    //so that, with every object the nextId will go up by one. And they will all have access to this increase because it is static and belongs to the class
+    //   this.id = nextId   (says that every object gets a unique id based off the nextId)          nextId++ (will increment the id number by 1 with each new object)
+    //IF NEXTID WASN'T STATIC THEN THE UPDATED ID# WOULD NOT WORK. EVERY OBJECT WOULD BE ID#1
     private static int nextId = 1;
 
 
+
+    //TODO:fields
     //? We can make sure that the field is required by using @NotBlank. This will ensure that whitespaces aren't counted as characters
     //? You can specify the min and max characters of the field input by user by using @Size
     //? You can add a specific message to help the user understand what they entered wrong
@@ -23,17 +32,21 @@ public class Event {
     private String name;
 
 
-    //? We are allowing the field to be blank if user decides, or up to 500 characters.
+    //? We are allowing the field to be blank if user decides, or up to 500 characters. By leaving off the min, we make the field OPTIONAL
     @Size(max = 500, message = "Description too long!")
     private String description;
 
 
     //? We added a contactEmail field using the @Email annotation-- specify that the string should conform to the rules that dictate common emails
     //? After creating the contactEmail field, we added it to the constructor and created getters and setters for it as well.
+    @NotBlank
     @Email(message = "Invalid email. Try again")
     private String contactEmail;
 
 
+
+
+    //TODO:constructor
     public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
@@ -42,43 +55,54 @@ public class Event {
         nextId++;
     }
 
-
+    //TODO:getter-name
     public String getName() {
         return name;
     }
 
+    //TODO:setter-name
     public void setName(String name) {
         this.name = name;
     }
 
+    //TODO:getter-description
     public String getDescription() {
         return description;
     }
 
+    //TODO:setter-description
     public void setDescription(String description) {
         this.description = description;
     }
 
-    //* My getters and setters included the @Email in them, but I removed since they weren't generated in the follow along video
+    //TODO: getter-contactEmail
+    //? My getters and setters included the @Email in them, but I removed since they weren't generated in the follow along video
     public String getContactEmail() {
         return contactEmail;
     }
 
+    //TODO: setter-contactEmail
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
 
+
+    //TODO: ONLY make a getter for id. We want to access the value, but never change it.
+    //and we don't need a getter or setter for nextId because we are just using it in the constructor to help create a unique id for each object of the class
     public int getId() {
         return id;
     }
 
 
 
+
+    //TODO:toString method
     @Override
     public String toString() {
         return name;
     }
 
+    //TODO: create equals and hashcode methods based on id ONLY
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -89,6 +113,6 @@ public class Event {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hashCode(id);
     }
 }
